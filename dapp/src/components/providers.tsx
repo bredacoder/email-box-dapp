2e5279843b733e067099128f0cbded5ddf6a6228
Daniel Breda <bredacoder@gmail.com>
2025-04-15 21:44:28 -0400

    feat: mode toggle transition

--- dapp/src/components/providers.tsx
+++ dapp/src/components/providers.tsx
@@ -3,10 +3,14 @@ import { TooltipProvider } from "./ui/tooltip";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
-        <TooltipProvider delayDuration={0}>
-            {children}
-        </TooltipProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+      enableColorScheme
+    >
+      <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
     </ThemeProvider>
-  )
+  );
 }
